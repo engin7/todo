a8e3faf3a6866591253b795ac8e40a358a8ca001
Engin KUK <kuk.engin@gmail.com>
2020-05-20 00:41:27 +0300

    User Interface Tests completed

--- ToDo/ToDoUITests/ToDoUITests.swift
+++ ToDo/ToDoUITests/ToDoUITests.swift
@@ -29,35 +29,66 @@ class ToDoUITests: XCTestCase {
          super.tearDown()
     }
 
+    
     func testInitialScreen() {
 
-         XCTAssert(app.tables["List todos"].exists)
+           XCTAssert(app.tables["List todos"].exists)
     }
     
     func testTapCell() {
-        // TODO: Remove the item you added
-        
-        let addButton = app.buttons["Add"]
-            addButton.tap()
-            let addAlert = app.alerts
-            let textField = addAlert.textFields.element
-            textField.typeText("Go to lunch")
-            app.tap()
-            let tableView = app.tables["List todos"]
-            let cell = tableView.cells.element(matching: .cell, identifier: "myCell_0")
-            XCTAssert(cell.exists && cell.isHittable)
-             
+          // incase list is empty
+            addMockCell()
+          
+           let tableView = self.app.tables["List todos"]
+           let cell = tableView.cells.element(matching: .cell, identifier: "myCell_0")
+           _ = cell.waitForExistence(timeout: 2)
+           XCTAssert(cell.exists && cell.isHittable)
+
+            removeMockCell()
        }
-     //  If you're doing only this test make sure have at least one item in DataBase.
-     func testAccessibilityButton() {
-           
+    
+      func testAccessibilityButton() {
+           // incase list is empty
+           addMockCell()
+
            let tableView = app.tables["List todos"]
            let cell = tableView.cells.element(matching: .cell, identifier: "myCell_0")
+           _ = cell.waitForExistence(timeout: 2)
            let accessibilityButton = cell.buttons["More Info"]
            XCTAssert(accessibilityButton.exists && accessibilityButton.isHittable)
             
+          removeMockCell()
+
           }
     
+    func testDisabledDeleteButton() {
+        
+        let toDoListNavigationBar = app.navigationBars["To-Do List"]
+        let deleteButton = toDoListNavigationBar.buttons["trash.circle.fill"]
+        XCTAssert(deleteButton.exists && !deleteButton.isEnabled)
+
+    }
+    
+    
+    func testDeleteButton() {
+        
+        // incase list is empty
+        addMockCell()
+        
+        let tableView = app.tables["List todos"]
+        let cell = tableView.cells.element(matching: .cell, identifier: "myCell_0")
+        _ = cell.waitForExistence(timeout: 2)
+         let toDoListNavigationBar = app.navigationBars["To-Do List"]
+        toDoListNavigationBar.buttons["Edit"].tap()
+        tableView.cells["myCell_0"].children(matching: .button).element.tap()
+        let deleteButton = toDoListNavigationBar.buttons["trash.circle.fill"]
+        XCTAssert(deleteButton.exists && deleteButton.isEnabled)
+        let doneButton = app.navigationBars["To-Do List"].buttons["Done"]
+        doneButton.tap()
+        
+        removeMockCell()
+        
+    }
 
     func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
@@ -67,5 +98,36 @@ class ToDoUITests: XCTestCase {
             }
         }
     }
-  
+    
+    // MARK: - Helper methods
+    
+    func addMockCell(){
+    
+        let addButton = app.buttons["Add"]
+        addButton.tap()
+        let addAlert = app.alerts
+        let textField = addAlert.textFields.element
+        textField.typeText("Go to lunch")
+        app.tap()
+    
+    }
+        
+    func removeMockCell(){
+        
+        let tableView = app.tables["List todos"]
+        let index = "myCell_" + String((tableView.cells.count) - 1 )
+ 
+        // Swipe down until it is visible   
+        while !tableView.cells[index].exists {
+            app.swipeUp()
+        }
+
+        let toDoListNavigationBar = app.navigationBars["To-Do List"]
+        toDoListNavigationBar.buttons["Edit"].tap()
+        tableView.cells[index].children(matching: .button).element.tap()
+        toDoListNavigationBar.buttons["trash.circle.fill"].tap()
+        
+    }
+    
 }
+
